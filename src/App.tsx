65454be550e27049598e665db03efafdb087ff46
Davi Rodrigues <davi.valdez.rodrigues@gmail.com>
2025-08-23 15:23:08 -0300

    feat(context): add TasksContext and TasksContextProvider for task management

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,25 @@
 import { Box, Flex, Heading } from "@radix-ui/themes"
 import { CreateTaskForm } from "./components/CreateTaskForm"
 import { TaskBoard } from "./components/TaskBoard"
+import { TasksContextProvider } from "./contexts/TasksContextProvider"
 
 function App() {
 
   return (
-    <Box maxWidth="80rem" mx="auto">
-      <Box height="4rem">
-        <Flex align="center" gap="4" height="100%">
-          <Heading as="h1" size="8" weight="light">React Kanban</Heading>
-          <CreateTaskForm />
-        </Flex>
+    <TasksContextProvider>
+      <Box maxWidth="80rem" mx="auto">
+        <Box height="4rem">
+          <Flex align="center" gap="4" height="100%">
+            <Heading as="h1" size="8" weight="light">React Kanban</Heading>
+            <CreateTaskForm />
+          </Flex>
+        </Box>
+        <Box>
+          <Heading as="h2" mb="4">Task Board</Heading>
+          <TaskBoard />
+        </Box>
       </Box>
-      <Box>
-        <Heading as="h2" mb="4">Task Board</Heading>
-        <TaskBoard />
-      </Box>
-    </Box>
+    </TasksContextProvider>
   )
 }
 
